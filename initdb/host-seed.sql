5158d3bfd0cba02164c8451d775d4d2873c9c9d7
Danniel Rolfe <talkto@dannielrolfe.com>
2019-05-18 01:05:13 -0700

    WIP

--- initdb/host-seed.sql
+++ initdb/host-seed.sql
@@ -2,42 +2,67 @@ CREATE DATABASE pandp;
 
 \connect pandp;
 
+-- Users is the general schema. A customer, host and admin are users.
 CREATE SCHEMA users;
 CREATE SCHEMA hosts;
+CREATE SCHEMA customer;
 
 CREATE TYPE permissionsE AS ENUM ('CUSTOMER', 'HOST', 'ADMIN');
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users.credentials (
-  id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+  id uuid DEFAULT uuid_generate_v4 (),
+  PRIMARY KEY(id),
   email_login VARCHAR(50) NOT NULL,
   UNIQUE(email_login),
-  password text NOT NULL,
+  password VARCHAR(155) NOT NULL,
   permissions permissionsE
  );
 
-INSERT INTO users.credentials (id, email_login, password, permissions) VALUES
-  (1, 'user@gmail.com', 'password', 'CUSTOMER'),
-  (2, 'host1@gmail.com', 'password', 'HOST'),
-  (3, 'host2@gmail.com', 'password', 'HOST'),
-  (4, 'host3@gmail.com', 'password', 'HOST'),
-  (5, 'host4@gmail.com', 'password', 'HOST'),
-  (6, 'admin@gmail.com', 'password', 'ADMIN');
+INSERT INTO users.credentials (email_login, password, permissions) VALUES
+  ( 'customer1@gmail.com', 'password', 'CUSTOMER'),
+  ( 'customer2@gmail.com', 'password', 'CUSTOMER'),
+  ( 'customer3@gmail.com', 'password', 'CUSTOMER'),
+  ( 'customer4@gmail.com', 'password', 'CUSTOMER'),
+  ( 'host1@gmail.com', 'password', 'HOST'),
+  ( 'host2@gmail.com', 'password', 'HOST'),
+  ( 'host3@gmail.com', 'password', 'HOST'),
+  ( 'host4@gmail.com', 'password', 'HOST'),
+  ( 'admin1@gmail.com', 'password', 'ADMIN'),
+  ( 'admin2@gmail.com', 'password', 'ADMIN'),
+  ( 'admin3@gmail.com', 'password', 'ADMIN'),
+  ( 'admin4@gmail.com', 'password', 'ADMIN');
 
 CREATE TABLE hosts.profile (
-  id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES users.credentials(id)
+  profile_id uuid,
+  UNIQUE(profile_id),
+--   PRIMARY KEY (profile_id),
+  FOREIGN KEY (profile_id) REFERENCES users.credentials(id),
+  first_name VARCHAR(55),
+  last_name VARCHAR(55),
+  mobile_number VARCHAR(20),
+  home_number VARCHAR(20),
+  profile_image VARCHAR(255),
+  description text
 );
 
-INSERT INTO hosts.profile (id) VALUES
-  (2),
-  (3),
-  (4);
+INSERT INTO hosts.profile (
+  first_name,
+  last_name,
+  mobile_number,
+  home_number,
+  profile_image,
+  description
+) VALUES
+  ('Danniel', 'Rolfe', '971-713-1358', '971-713-1359', 'https://via.placeholder.com/350x350', 'The boogy man'),
+  ('Denise', 'Rolfe', '971-713-1358', '971-713-1358', 'https://via.placeholder.com/350x350', 'The boogy man'),
+  ('Alex', 'Rolfe', '971-713-1358', '971-713-1358', 'https://via.placeholder.com/350x350', 'The boogy man'),
+  ('Futuer', 'Rolfe', '971-713-1358', '971-713-1358', 'https://via.placeholder.com/350x350', 'The boogy man');
 
 CREATE TABLE hosts.property (
-  id INT,
-  UNIQUE(id),
-  FOREIGN KEY (id) REFERENCES hosts.profile(id) ON DELETE CASCADE,
+  property_id uuid,
+  UNIQUE(property_id),
+  FOREIGN KEY (property_id) REFERENCES hosts.profile(profile_id),
   property_rating INT Array,
   property_latitude DECIMAL,
   property_longitude DECIMAL,
@@ -48,7 +73,6 @@ CREATE TABLE hosts.property (
 );
 
 INSERT INTO hosts.property (
-  id,
   property_rating,
   property_latitude,
   property_longitude,
@@ -57,23 +81,34 @@ INSERT INTO hosts.property (
   property_state,
   property_zip
 ) VALUES
-  (2, Array[2, 4, 5, 1 ], 45.512794, -122.679565, '1111 SW 7th', 'Portland', 'OR', 97124),
-  (3, Array[2, 4, 5, 1 ], 45.512794, -122.679565, '1111 SW 7th', 'Portland', 'OR', 97124),
-  (4, Array[2, 4, 5, 1 ], 45.512794, -122.679565, '1111 SW 7th', 'Portland', 'OR', 97124);
+  (Array[2, 4, 5, 1 ], 45.512794, -122.679565, '1111 SW 7th', 'Portland', 'OR', 97124),
+  (Array[2, 4, 5, 1 ], 45.512794, -122.679565, '1111 SW 7th', 'Portland', 'OR', 97124),
+  (Array[2, 4, 5, 1 ], 45.512794, -122.679565, '1111 SW 7th', 'Portland', 'OR', 97124);
 
 CREATE TABLE hosts.schedule (
-  id INT,
-  FOREIGN KEY (id) REFERENCES hosts.property(id),
+  property_schedule_id uuid,
+  UNIQUE(property_schedule_id),
+  FOREIGN KEY (property_schedule_id) REFERENCES hosts.property(property_id),
   property_schedule_checkin DATE,
   property_schedule_checkout DATE
 );
 
 INSERT INTO hosts.schedule (
-  id,
   property_schedule_checkin,
   property_schedule_checkout
 ) VALUES
-  (2, '9999-12-31 23:59:59', '9999-12-31 23:59:59'),
-  (3, '9999-12-31 23:59:59', '9999-12-31 23:59:59'),
-  (4, '9999-12-31 23:59:59', '9999-12-31 23:59:59');
+  ('9999-12-31 23:59:59', '9999-12-31 23:59:59'),
+  ('9999-12-31 23:59:59', '9999-12-31 23:59:59'),
+  ('9999-12-31 23:59:59', '9999-12-31 23:59:59');
+
+CREATE TABLE customer.profile(
+  customer_profile_id uuid,
+  UNIQUE(customer_profile_id),
+  PRIMARY KEY (customer_profile_id),
+  FOREIGN KEY (customer_profile_id) REFERENCES users.credentials(id)
+);
+
+-- INSERT INTO customer.profile (
+--   id
+-- ) VALUES (1);
 
